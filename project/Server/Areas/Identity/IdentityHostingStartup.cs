08cbad5551389496a4a20f501a68d5f8a4196dd8
ONG JUN KANG 2000798E <2000798E@Student.tp.edu.sg>
2021-12-18 21:57:47 +0800

    Added database and default data

---
+++ project/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using project.Server.Data;
+using project.Server.Models;
+
+[assembly: HostingStartup(typeof(project.Server.Areas.Identity.IdentityHostingStartup))]
+namespace project.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
