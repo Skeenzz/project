7c37e5bcf88c9a431c88fa5e76ea380cf6d54285
Skeen <2004143J@student.tp.edu.sg>
2022-02-09 17:19:46 +0800

    fin

--- project/Shared/Domain/FlightBooking.cs
+++ project/Shared/Domain/FlightBooking.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace project.Shared.Domain
 {
     public class FlightBooking : BaseDomainModel
     {
+        
+        [Required]
         public int Passenger { get;set; }
+        [Required]
         public int? FlightsId { get; set; }
         public virtual Flights Flights { get; set; }
+        [Required]
         public DateTime Departing { get; set; }
+        [Required]
         public DateTime Returning { get; set; }
     }
 }
